fadab7e4c42910878559d39473dea75b9d97885b
Savanna Wolvin <savanna.wolvin@gmail.com>
2021-09-24 15:32:44 -0600

    add run.sh files

---
+++ decisionTree/categorical_decision_tree/run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Savanna Wolvin
+# Created: 09/24/21
+
+# run decisionTree_categorical_main.py
+
+python3.8 decisionTree_categorical_main.py
---
+++ decisionTree/numerical_decision_tree/run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Savanna Wolvin
+# Created: 09/24/21
+
+# run decisionTree_numerical_main.py
+
+python3.8 decisionTree_numerical_main.py

a4da31414ca1c0209808b609840520366392ffe9
Savanna Wolvin <savanna.wolvin@gmail.com>
2021-11-13 14:18:07 -0700

    update gaussian kernal SVM

--- svm/dual_svm_gaussianKernal.py
+++ svm/dual_svm_gaussianKernal.py
@@ -61,32 +61,20 @@ y2 = testData[:,4]
 
 #%% Functions
 
-def objective_func(a, gammax):
-    component1 = 0
-    kernal = 0
+def objective_func(a, kernal):    
+    cmp_ay = np.reshape(np.multiply(a, y), (-1, 1))
+    cmp_ayay = np.matmul(cmp_ay, cmp_ay.T)
+    comp_1 = np.multiply(cmp_ayay, kernal)
+    comp_1 = (1/2)*np.sum(comp_1)
     
-    for i in range(np.shape(a)[0]):
-        for j in range(np.shape(a)[0]):
-            kernalx = np.linalg.norm(x[i,:] - x[j,:])**2
-            kernalx = -(kernalx / gammax)
-            kernal = np.exp(kernalx) 
-            component1 += a[j]*y[j]*a[i]*y[i]*kernal
-            
-            kernal = 0
+    comp_2 = np.sum(a)
     
-    component1 = (1/2)*component1
-    # summation1 = np.sum(np.array([(a[i]*y[i]) for i in range(np.shape(a)[0])]), axis=0)
-    # summation2 = np.sum(np.array([(a[j]*y[j]) for j in range(np.shape(a)[0])]), axis=0)
-    
-    # component1 = summation1*summation2*kernal
-     
-    component2 = np.sum(a)
-    
-    return component1 - component2
+    return comp_1 - comp_2
 
 
 def equality_constraint(a):
-    iteration = [(a[i]*y[i]) for i in range(np.shape(a)[0])]
+    # iteration = [(a[i]*y[i]) for i in range(np.shape(a)[0])]
+    iteration = np.multiply(a, y)
     
     return np.sum(iteration)
 
@@ -102,77 +90,61 @@ for Cx in C:
         # Bounds
         bnds = [(0, (Cx/873))] * np.shape(trainData)[0]
         
-        
-        # Calculate Kernal
-        # kernal = 0
-        
-        # for i in range(np.shape(x)[0]):
-        #     for j in range(np.shape(x)[0]):
-        #         kernalx = np.linalg.norm(x[i,:] - x[j,:])**2
-        #         kernalx = -(kernalx / gammax)
-        #         kernal += np.exp(kernalx)          
-        
-        
         # Calculate Minimization
         print('Calculate Alpha Values for C = ' + str(Cx) + '/' + str(873) + ' and gamma = ' + str(gammax) + '...')
         
-        a0 = [0] * np.shape(x)[0]
-        
-        start_time = datetime.now()
-        # result = spo.minimize(objective_func, a0, args=(kernal), method='SLSQP', bounds=bnds, constraints=[constraint1])
-        result = spo.minimize(objective_func, a0, args=(gammax), method='SLSQP', bounds=bnds, constraints=[constraint1])
-        end_time = datetime.now()
-        
-        print(result.message + ': ' + 'Duration: ' + str(end_time - start_time))
-        
-        
-        # Calculate Weighted Vector and Bias
-        a = result.x
-        
-        train_prediction = []
+        # Calculate Kernal Array
+        kernal = np.zeros([np.shape(x)[0], np.shape(x)[0]])
         for i in range(np.shape(x)[0]):
-            kernal = 0
             for j in range(np.shape(x)[0]):
                 kernalx = np.linalg.norm(x[i,:] - x[j,:])**2
                 kernalx = -(kernalx / gammax)
-                kernal += np.exp(kernalx)
+                kernal[i, j] = np.exp(kernalx) 
+        
+        a0 = [0] * np.shape(x)[0]
+        
+        try:
+            start_time = datetime.now()
+            result = spo.minimize(objective_func, a0, args=(kernal), method='SLSQP', bounds=bnds, constraints=[constraint1], options={'disp': True})
+            end_time = datetime.now()
+            
+            print(result.message + ': ' + 'Duration: ' + str(end_time - start_time))
+             
+            # Calculate Weighted Vector and Bias
+            a = result.x
+            
+            train_prediction = []
+            ay = np.multiply(a, y)
+            for j in range(np.shape(x)[0]):
+                sum_ayk = np.sum(np.multiply(ay, kernal[:,j]))
+                train_prediction.append(np.sign(sum_ayk))
                 
-            train_prediction.append(a[i]*y[i]*kernal)
-        
-        train_prediction = np.sign(train_prediction)
-        train_error = [0]
-        [train_error.append(1) for i in range(np.shape(y)[0]) if y[i] != train_prediction[i]]
-        train_error = np.sum(train_error) / np.shape(y)[0]
-        
-        print('Training Error: ' + str(train_error))
-        
-        test_prediction = []
-        for i in range(np.shape(x)[0]):
-            kernal = 0
+            train_error = [0]
+            [train_error.append(1) for i in range(np.shape(y)[0]) if y[i] != train_prediction[i]]
+            train_error = np.sum(train_error) / np.shape(y)[0]
+            
+            print('Training Error: ' + str(train_error))
+            
+            
+            test_prediction = []
+            kernal_test = np.zeros([np.shape(x)[0], np.shape(x2)[0]])
+            for i in range(np.shape(x)[0]):
+                for j in range(np.shape(x2)[0]):
+                    kernalx = np.linalg.norm(x[i,:] - x2[j,:])**2
+                    kernalx = -(kernalx / gammax)
+                    kernal_test[i, j] = np.exp(kernalx) 
+            
             for j in range(np.shape(x2)[0]):
-                kernalx = np.linalg.norm(x[i,:] - x2[j,:])**2
-                kernalx = -(kernalx / gammax)
-                kernal += np.exp(kernalx)
-                
-            test_prediction.append(a[i]*y[i]*kernal)
-        
-        # for j in range(np.shape(x2)[0]):
-        #     kernal = 0
-        #     for i in range(np.shape(x)[0]):
-        #         kernalx = np.linalg.norm(x[i,:] - x2[j,:])**2
-        #         kernalx = -(kernalx / gammax)
-        #         kernal += np.exp(kernalx)
-                
-        #     test_prediction.append(a[i]*y[i]*kernal)
-        
-        
-        test_prediction = np.sign(test_prediction)
-        test_error = [0]
-        [test_error.append(1) for i in range(np.shape(y2)[0]) if y2[i] != test_prediction[i]]
-        test_error = np.sum(test_error) / np.shape(y2)[0]
-        
-        
-        print('Test Error: ' + str(test_error))
+                sum_ayk = np.sum(np.multiply(ay, kernal_test[:,j]))
+                test_prediction.append(np.sign(sum_ayk))
+            
+            test_error = [0]
+            [test_error.append(1) for i in range(np.shape(y2)[0]) if y2[i] != test_prediction[i]]
+            test_error = np.sum(test_error) / np.shape(y2)[0]
+            
+            print('Test Error: ' + str(test_error))
+        except: 
+            print('Failed for Alpha Values for C = ' + str(Cx) + '/' + str(873) + ' and gamma = ' + str(gammax) + '...')
         
         
 

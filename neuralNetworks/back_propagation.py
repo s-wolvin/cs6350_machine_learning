36d205982b1be4312e3d57607f4960c550165494
Savanna Wolvin <savanna.wolvin@gmail.com>
2021-11-30 19:14:45 -0700

    start neural network

---
+++ neuralNetworks/back_propagation.py
@@ -0,0 +1,37 @@
+## Savanna Wolvin
+# Created: Nov. 29th, 2021
+# Edited: 
+    
+# SUMMARY
+
+# INPUT
+
+# OUTPUT
+
+
+#%% Global imports
+import numpy as np
+import pandas as pd
+
+
+
+
+#%% Create Data
+    
+z_hidden_layer = np.array([[1,0.00247,0.99753],[1,0.01803,0.98197]])
+
+weights = [{'z_1_1':[-1,-2,-3],'z_2_1':[1,2,3]},{'z_1_2':[-1,-2,-3],'z_2_2':[1,2,3]},{'y':[-1,2,-1.5]}]
+
+y = -2.43689
+
+
+
+
+#%% Calculate the loss gradient
+
+for 
+
+
+
+
+
---
+++ neuralNetworks/stochastic_gradient_descent_nn.py
@@ -0,0 +1,87 @@
+## Savanna Wolvin
+# Created: Nov. 29th, 2021
+# Edited: 
+    
+# SUMMARY
+
+# INPUT
+
+# OUTPUT
+
+
+#%% Global Inputs
+import numpy as np
+import pandas as pd
+import random as rd
+
+
+
+
+#%% Variable Presets
+# Data set
+data_file_train = 'train'
+data_file_test = 'test'
+
+data_folder = 'bank-note/'
+
+max_epoch = 50
+
+lr_0 = 0.1
+
+width = 5
+depth = 2
+
+
+
+#%% Load Data
+    
+trainData = pd.read_csv(data_folder + data_file_train + '.csv', sep=',', header=None)
+trainData = trainData.to_numpy()
+trainData[np.where(trainData[:,4] == 0), 4] = -1
+
+testData = pd.read_csv(data_folder + data_file_test + '.csv', sep=',', header=None)
+testData = testData.to_numpy()
+testData[np.where(testData[:,4] == 0), 4] = -1
+
+
+
+#%% Sigmoid Function
+
+def sigmoid(z):
+    s = 1 / (1 + np.exp(-z))
+    return s
+
+
+#%%  Initialize the layers
+
+z_hidden_layer = np.random.normal(0, 1, (depth, width))
+
+
+
+weighted_vector = np.ones((np.shape(trainData)[1]-1))
+
+for t in range(max_epoch):
+    # shuffle dataset
+    rand_idx = rd.sample(range(0, np.shape(trainData)[0]), np.shape(trainData)[0])
+    trainDataX = trainData[rand_idx, :]
+    
+    for ex in range(np.shape(trainDataX)[0]):
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

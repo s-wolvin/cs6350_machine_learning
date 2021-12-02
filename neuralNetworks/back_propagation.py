91625f1cb250d246d839a421a75a8571329b69b4
Savanna Wolvin <savanna.wolvin@gmail.com>
2021-12-01 17:03:54 -0700

    another attempt at backward propagation

--- neuralNetworks/back_propagation.py
+++ neuralNetworks/back_propagation.py
@@ -1,6 +1,6 @@
 ## Savanna Wolvin
 # Created: Nov. 29th, 2021
-# Edited: 
+# Edited: Dec. 1st, 2021
     
 # SUMMARY
 
@@ -11,25 +11,122 @@
 
 #%% Global imports
 import numpy as np
-import pandas as pd
 
 
 
 
 #%% Create Data
-    
-z_hidden_layer = np.array([[1,0.00247,0.99753],[1,0.01803,0.98197]])
 
-weights = [{'z_1_1':[-1,-2,-3],'z_2_1':[1,2,3]},{'z_1_2':[-1,-2,-3],'z_2_2':[1,2,3]},{'y':[-1,2,-1.5]}]
+w_layer = {}
+w_layer[0] = np.array([[-2,-3],[2,3]])
+w_layer[1] = np.array([[-2,-3],[2,3]])
+w_layer[2] = np.array([[2,-1.5]])
+
+b_layer = {}
+b_layer[0] = np.array([[-1],[1]])
+b_layer[1] = np.array([[-1],[1]])
+b_layer[2] = np.array([[-1]])
+
+z_layer = {}
+z_layer[1] = np.array([1,0.00247,0.99753])
+z_layer[2] = np.array([1,0.01803,0.98197])
 
 y = -2.43689
+y_star = 1
+width = 3
+depth = 2
+X_rand_ex = np.asarray([1,1,1])
 
 
 
 
 #%% Calculate the loss gradient
 
-for 
+def backward_prop(y_star, y, z_layer, X_rand_ex):
+    gradient_loss = {}
+    z_layer[0] = X_rand_ex
+    
+    cashe_array = np.asarray(y - y_star) # cashe this value
+    
+    # output layer
+    gradient_loss[depth] = np.expand_dims(cashe_array * z_layer[depth], axis=0)
+    cashe_array = cashe_array.T
+    
+    # z = 2
+    cashe_array = cashe_array * w_layer[2] # dL/dy * dy/dz
+    cashe_array_z_1_z = cashe_array * z_layer[2][1:] * (z_layer[2][1:] - 1)
+    # cashe_array_z_1_z = np.reshape(cashe_array_z_1_z, (1,-1))
+    z_layer_x = np.tile(np.expand_dims(z_layer[2-1], axis=1), np.shape(cashe_array_z_1_z)[0])  
+    
+    gradient_loss[2-1] = z_layer_x.dot(cashe_array_z_1_z).T
+    
+    # z = 1
+    cashe_array = cashe_array.T * w_layer[1] # dL/dy * dy/dz
+    cashe_array_z_1_z = cashe_array * z_layer[1][1:] * (z_layer[1][1:] - 1)
+    # cashe_array_z_1_z = np.reshape(cashe_array_z_1_z, (1,-1))
+    z_layer_x = np.tile(np.expand_dims(z_layer[1-1], axis=1), np.shape(cashe_array_z_1_z)[0])   
+    
+    gradient_loss[1-1] = z_layer_x.dot(cashe_array_z_1_z).T
+    
+    
+    ###################################################################
+    
+    
+    # initial variables
+    gradient_loss_1 = {}
+    cashe_array = np.asarray(y - y_star) # cashe this value
+    
+    # Calculate output layer
+    gradient_loss_1[depth] = np.expand_dims(cashe_array * z_layer[depth], axis=0)
+
+    # loop through hidden layers
+    for lx in range(depth, 0, -1):
+        print(lx)
+        cashe_array = cashe_array.T * w_layer[lx] # dL/dy * dy/dz
+        cashe_array_z_1_z = cashe_array * z_layer[lx][1:] * (z_layer[lx][1:] - 1)
+        z_layer_x = np.tile(np.expand_dims(z_layer[lx-1], axis=1), np.shape(cashe_array_z_1_z)[0]) 
+        
+        gradient_loss_1[lx-1] = z_layer_x.dot(cashe_array_z_1_z).T
+        
+    
+    return gradient_loss, gradient_loss_1
+
+
+
+#%%
+
+gradientLoss, gradient_loss_1 = backward_prop(y_star, y, z_layer, X_rand_ex)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- neuralNetworks/stochastic_gradient_descent_nn.py
+++ neuralNetworks/stochastic_gradient_descent_nn.py
@@ -1,6 +1,6 @@
 ## Savanna Wolvin
 # Created: Nov. 29th, 2021
-# Edited: 
+# Edited: Dec. 1st, 2021
     
 # SUMMARY
 
@@ -28,8 +28,9 @@ max_epoch = 50
 
 lr_0 = 0.1
 
-width = 5
+width = 3
 depth = 2
+
 
 
 
@@ -47,6 +48,28 @@ testData[np.where(testData[:,4] == 0), 4] = -1
 
 
 
+
+#%% create an array of sizes
+
+num_nodes = [np.shape(X)[1]]
+
+for layerx in range(depth):
+    num_nodes.append(width-1)
+
+num_nodes.append(1)
+
+
+
+
+#%% Learning Rate
+
+def learnRate(t):
+    lr = lr_0 / ( 1 + ( ( lr_0 / depth ) * t ) )
+    return lr
+
+
+
+
 #%% Sigmoid Function
 
 def sigmoid(z):
@@ -54,39 +77,143 @@ def sigmoid(z):
     return s
 
 
+
+
 #%%  Initialize the layers
 
-def initialize_layers(initial_width):
+def initialize_layers():
     w_layer = {}
+    b_layer = {}
 
-    for dx in range(depth):
-        w_layer[dx] = np.append(np.array(1), np.random.normal(0, 1, (1, width-1)))
+    for dx in range(depth+1):
+        w_layer[dx] = np.random.normal(0, 1, (num_nodes[dx+1], num_nodes[dx]))
+        b_layer[dx] = np.ones((num_nodes[dx+1], 1))
     
-    return w_layer
+    return w_layer, b_layer
 
 
 
 
 #%% Forward propagation
 
-def forward_prop(z_layer, X):
-    for dx in range(depth):
+def forward_prop(X_rand_ex):
+    z_layer = {} 
+    
+    z = w_layer[0].dot(X_rand_ex) + b_layer[0]
+    Z1 = sigmoid(z)
         
+    for dx in range(1,depth+1):
+        z_layer[dx] = np.append(1, Z1)
+        z = w_layer[dx].dot(Z1) + b_layer[dx]
+        Z1 = sigmoid(z)
     
-    
-    
-    
-    
+    return z, z_layer
 
-weighted_vector = np.ones((np.shape(trainData)[1]-1))
 
+
+
+#%% Backward Propagation
+
+def backward_prop(y_star, y, z_layer, X_rand_ex):
+    # gradient_loss = {}
+    z_layer[0] = X_rand_ex
+    
+    # cashe_array = (y - y_star) # cashe this value
+    
+    # # output layer
+    # dL_output = cashe_array * z_layer[depth]
+    # gradient_loss[depth] = dL_output
+    
+    # # z = 2
+    # cashe_array = cashe_array.T * w_layer[2] # dL/dy * dy/dz
+    # cashe_array_z_1_z = cashe_array * z_layer[2][1:] * (1 - z_layer[2][1:])
+    # gradient_loss[2-1] = np.expand_dims(z_layer[2-1], axis=1).dot(cashe_array_z_1_z).T
+    
+    # # z = 1
+    # cashe_array = cashe_array.T * w_layer[1] # dL/dy * dy/dz
+    # cashe_array_z_1_z = cashe_array * z_layer[1][1:] * (1 - z_layer[1][1:])
+    # gradient_loss[1-1] = np.expand_dims(z_layer[1-1], axis=1).dot(cashe_array_z_1_z).T
+    
+    # initial variables
+    gradient_loss_1 = {}
+    cashe_array = np.array(y - y_star) # cashe this value
+    
+    # Calculate output layer
+    gradient_loss_1[depth] = cashe_array * z_layer[depth]
+
+    # loop through hidden layers
+    for lx in range(depth, 0, -1):
+        print(lx)
+        cashe_array = cashe_array.T * w_layer[lx] # dL/dy * dy/dz
+        cashe_array_z_1_z = cashe_array * z_layer[lx][1:] * (z_layer[lx][1:] - 1)
+        z_layer_x = np.tile(np.expand_dims(z_layer[lx-1], axis=1), np.shape(cashe_array_z_1_z)[0]) 
+        
+        gradient_loss_1[lx-1] = z_layer_x.dot(cashe_array_z_1_z).T
+    
+    return gradient_loss_1
+
+
+
+
+#%% Main of the Neural Network
+
+w_layer, b_layer = initialize_layers() # create data arrays to hold the weight 
+                                       # vectors and the bias values
+
+# Number of times to loop through the entire dataset, change to stop at a 
+# certain value of loss
 for t in range(max_epoch):
     # shuffle dataset
-    rand_idx = rd.sample(range(0, np.shape(trainData)[0]), np.shape(trainData)[0])
-    trainDataX = trainData[rand_idx, :]
+    rand_idx = rd.sample(range(0, np.shape(X)[0]), np.shape(X)[0])
+    X_rand = X[rand_idx, :]
+    Y_rand = Y[rand_idx]
     
-    # for ex in range(np.shape(trainDataX)[0]):
+    learning_rate = learnRate(t) # learing rate changes every epoch
+    
+    # loop through each example of the dataset as if it's its own dataset
+    for ex in range(np.shape(X_rand)[0]):
+        X_rand_ex = np.expand_dims(X_rand[ex,:], axis=1)
         
+        # Forward Propagation
+        y, z_layer = forward_prop(X_rand_ex)
+        
+        # Backward Propagation
+        gradientLoss = backward_prop(Y_rand[ex], y, z_layer, X_rand_ex)
+        
+        # edit wight vector by the back propagation
+        w_layer = w_layer - learning_rate * gradientLoss
+        b_layer = b_layer - learning_rate * gradientLoss
+        
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
